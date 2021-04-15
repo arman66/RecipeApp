151dc209d30001543fd2fcff32fb6ef0670d6a8c
arman66 <ardo.acosta@gmail.com>
2021-04-15 10:35:29 -0600

    prepare for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,24 @@
+import path from 'path'
 import express from 'express'
 import dotenv from 'dotenv'
 
 dotenv.config()
 const app = express()
 
-
-
 app.use(express.json())
 
 const PORT = process.env.PORT || 5000
 
-app.get('/', (req, res) => {
-  res.send('API is running....')
+app.listen(PORT, () =>
+  console.log(`Server is running in ${process.env.NODE_ENV} on port ${PORT}`)
+)
+
+const __dirname = path.resolve()
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')))
+}
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
 })
-
-
-app.listen(PORT, () => console.log(`Server is running in ${process.env.NODE_ENV} on port ${PORT}`))
